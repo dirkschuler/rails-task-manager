1bfe1829db6cf2eba29d857449ccfcc369590299
Dirk Schuler <dscbdv@gmail.com>
2017-09-05 14:54:19 +0200

    To do list exercise complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create(name: 'Shopping', description: 'Butter and milk')
+Task.create(name: 'Clean', description: 'Windows')
